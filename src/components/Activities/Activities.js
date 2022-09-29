616af4235fa55a61475c698313b5383f95a19735
masumbillah360 <masumbillah6584@gmail.com>
2022-09-29 15:06:12 +0600

    set activity title and added gap on article

--- src/components/Activities/Activities.js
+++ src/components/Activities/Activities.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Activity from "../Activity/Activity";
-import './Acitvities.css'
+import "./Acitvities.css";
 
 const Activities = () => {
   const [datas, setDatas] = useState([]);
@@ -11,8 +11,13 @@ const Activities = () => {
   }, []);
 
   return (
-    <div className="article-container">
-        {datas.map(data=><Activity key={data.id} data = {data}></Activity>)}
+    <div>
+      <h4>Select Today's Activity</h4>
+      <div className="article-container">
+        {datas.map((data) => (
+          <Activity key={data.id} data={data}></Activity>
+        ))}
+      </div>
     </div>
   );
 };
